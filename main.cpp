86f61c5881e0dccffd1e315a7717cc34e22edf2d
sara.viegas <sara.viegas@pop-rn.rnp.br>
2023-05-25 17:35:16 -0300

    asas | asas - centro

--- main.cpp
+++ main.cpp
@@ -42,33 +42,59 @@ int main()
     draw.putBox(5, 20, 50, 51, 54, 55);
     draw.putBox(5, 20, 50, 51, 46, 47);
 
-    // asas direita
+    // asa direita
 
     draw.putEllipsoid(30,42,50,20,5,6);
     draw.cutEllipsoid(30,42,50,6,6,6);
+
     draw.cutBox(5, 50, 38, 47, 50, 51);
+
     draw.putBox(25, 35, 40, 45, 45, 47);
     draw.putBox(25, 35, 40, 45, 54, 56);
     draw.cutBox(29, 32, 40, 45, 54, 55);
     draw.cutBox(29, 32, 40, 45, 46, 47);
-
+    
     draw.putBox(50, 58, 41, 44, 49, 50);
     draw.putBox(49, 52, 41, 44, 48, 49);
-
     draw.putBox(50, 58, 41, 44, 51, 52);
     draw.putBox(49, 52, 41, 44, 52, 53);
 
+    // asas direita -3 pinos
+    draw.putBox(25, 26, 34, 48, 50, 51);
+    draw.putBox(32, 33, 34, 48, 53, 54);
+    draw.putBox(32, 33, 34, 48, 47, 48);
 
+    // asas direita - centro-centro
+    draw.putSphere(30, 42, 50, 4);
+    draw.cutSphere(30, 42, 50, 2);
+    draw.cutBox(28, 33, 39, 41, 48, 53);
 
     // asa esquerda
 
-    // draw.putEllipsoid(30,42,50,20,5,6);
-    // draw.cutEllipsoid(30,42,50,6,6,6);
-    // draw.cutBox(5, 50, 38, 47, 50, 51);
-    // draw.putBox(25, 35, 40, 45, 45, 47);
-    // draw.putBox(25, 35, 40, 45, 54, 56);
-    // draw.cutBox(29, 32, 40, 45, 54, 55);
-    // draw.cutBox(29, 32, 40, 45, 46, 47);
+    draw.putEllipsoid(30,67,50,20,5,6);
+    draw.cutEllipsoid(30,67,50,6,6,6);
+
+    draw.cutBox(5, 50, 63, 72, 50, 51);
+
+    draw.putBox(25, 35, 65, 70, 45, 47);
+    draw.putBox(25, 35, 65, 70, 54, 56);
+    draw.cutBox(29, 32, 65, 70, 54, 55);
+    draw.cutBox(29, 32, 65, 70, 46, 47);
+
+    draw.putBox(50, 58, 66, 69, 49, 50);
+    draw.putBox(49, 52, 66, 69, 48, 49);
+    draw.putBox(50, 58, 66, 69, 51, 52);
+    draw.putBox(49, 52, 66, 69, 52, 53);
+
+    // asas esquerda -3 pinos
+    draw.putBox(25, 26, 62, 76, 50, 51);
+    draw.putBox(32, 33, 62, 76, 53, 54);
+    draw.putBox(32, 33, 62, 76, 47, 48);
+
+    // asas esquerda - centro-centro
+    draw.putSphere(30, 67, 50, 4);
+    draw.cutSphere(30, 67, 50, 2);
+    draw.cutBox(28, 33, 69, 71, 48, 53);
 
     draw.writeOFF("zariman.off");
 
